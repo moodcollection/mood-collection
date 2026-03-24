3ffae33ddc94d53abd615399ffcf1361798dc76b
moodcollection <info@mymoodcollection.co.uk>
2026-03-24 22:30:22 +0000

    Update create-checkout.js

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -1,1 +1,44 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  try {
+    const { items } = JSON.parse(event.body);
+
+    const lineItems = items.map(item => ({
+      price_data: {
+        currency: 'gbp',
+        product_data: {
+          name: item.name,
+          description: item.description,
+        },
+        unit_amount: Math.round(item.price * 100),
+      },
+      quantity: item.quantity,
+    }));
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: 'https://mymoodcollection.co.uk/thank-you.html',
+      cancel_url: 'https://mymoodcollection.co.uk/shop.html',
+      shipping_address_collection: {
+        allowed_countries: ['GB'],
+      },
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ url: session.url }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
